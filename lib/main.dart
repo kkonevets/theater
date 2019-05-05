c254198abc2fc95e2878841afc4e9dcfe4a50fb0
Kirill Konevets <kkonevets@gmail.com>
2019-05-05 19:40:36 +0300

    all

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Theater {
+  Theater({this.name, this.time, this.totalSeats});
+
+  String name;
+  TimeOfDay time;
+  int totalSeats;
+}
+
+List<Theater> _theaters = [
+  Theater(name: "Сеанс 1", time: TimeOfDay(hour: 10, minute: 20)),
+  Theater(name: "Сеанс 2", time: TimeOfDay(hour: 13, minute: 0)),
+  Theater(name: "Сеанс 3", time: TimeOfDay(hour: 15, minute: 45)),
+];
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -22,11 +36,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  void _incrementCounter() {
+  // #docregion _buildSuggestions
+  Widget _buildTheaters() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _theaters.length * 2 - 1, // including dividers
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          return _buildRow(_theaters[index]);
+        });
+  }
+
+  Widget _dialogBuilder(BuildContext context, Theater theater) {
+    return SimpleDialog(children: <Widget>[
+      Container(
+        width: 80,
+        height: 80,
+      )
+    ]);
+  }
+
+  Widget _buildRow(Theater theater) {
+    return GestureDetector(
+        onTap: () => showDialog(
+            context: context,
+            builder: (context) => _dialogBuilder(context, theater)),
+        child: ListTile(
+          title: Text(
+            theater.name,
+            style: _biggerFont,
+          ),
+        ));
+  }
+
+  void _listItemAdder() {
     setState(() {
-      _counter++;
+      // _counter++;
     });
   }
 
@@ -36,25 +85,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
-          ),
-          ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
-          ),
-        ],
-      ),
+      body: _buildTheaters(),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _listItemAdder,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
