c69d019d0c92e49cb3cda293fec8c86eed050c8a
Kirill Konevets <kkonevets@gmail.com>
2019-05-07 20:19:46 +0300

    all

--- lib/session.dart
+++ lib/session.dart
@@ -15,30 +15,30 @@ class Session {
 }
 
 List<Client> _clients = [
-  Client(
-      name: "Вася",
-      barcode: "234543432565",
-      phoneNumber: "+79261457894",
-      time: DateTime.parse("2019-07-20 20:18:04Z"),
-      seatNumber: 44),
-  Client(
-      name: "Петя",
-      barcode: "23436345254",
-      phoneNumber: "+79241557694",
-      time: DateTime.parse("2019-07-20 20:19:04Z"),
-      seatNumber: 14),
-  Client(
-      name: "Маша",
-      barcode: "96968565849",
-      phoneNumber: "+79061447824",
-      time: DateTime.parse("2019-07-20 20:12:04Z"),
-      seatNumber: 3),
-  Client(
-      name: "Света",
-      barcode: "25958438573475",
-      phoneNumber: "+7999345333",
-      time: DateTime.parse("2019-07-20 20:20:04Z"),
-      seatNumber: 2)
+  // Client(
+  //     name: "Вася",
+  //     barcode: "234543432565",
+  //     phoneNumber: "+79261457894",
+  //     time: DateTime.parse("2019-07-20 20:18:04Z"),
+  //     seatNumber: 44),
+  // Client(
+  //     name: "Петя",
+  //     barcode: "23436345254",
+  //     phoneNumber: "+79241557694",
+  //     time: DateTime.parse("2019-07-20 20:19:04Z"),
+  //     seatNumber: 14),
+  // Client(
+  //     name: "Маша",
+  //     barcode: "96968565849",
+  //     phoneNumber: "+79061447824",
+  //     time: DateTime.parse("2019-07-20 20:12:04Z"),
+  //     seatNumber: 3),
+  // Client(
+  //     name: "Света",
+  //     barcode: "25958438573475",
+  //     phoneNumber: "+7999345333",
+  //     time: DateTime.parse("2019-07-20 20:20:04Z"),
+  //     seatNumber: 2)
 ];
 
 class SessionRoute extends StatefulWidget {
